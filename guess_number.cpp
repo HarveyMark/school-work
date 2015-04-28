255b3afac6a520a99bed00e44c33d813ce63cdff
harveymark <harveymark1994@gmail.com>
2015-04-28 15:17:05 -0500

    test

---
+++ guess_number.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+		
+   int number; 	
+   int userNumber;	
+   string keepGoing;  
+
+   
+   srand((unsigned)time(NULL)); 
+   number = (rand() % 100) + 1; 
+   	
+  
+   cout << "Do you want to guess a number? Enter y or n: " << endl;
+   cin >> keepGoing;
+
+while(keepGoing == "y")
+{
+      
+    cout << "Try to guess by entering a number between 1 and 100: ";
+    cin >> userNumber; 
+     
+  if(userNumber > number)
+	{
+	cout << "\nLower!" << endl;
+	}
+	else
+	{
+	cout << "\nHigher!" << endl;
+	}
+
+  if(userNumber == number)
+  {
+    keepGoing = "n"; 
+    cout << "You are a genius. That's correct!\n";
+  }
+  else
+      {
+        cout << "Try again!" << endl;
+      }
+
+}
+      
+     
+    return 0;
+} 
